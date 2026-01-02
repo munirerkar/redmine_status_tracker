db40971bef10d2b3cf0c96254ab0084c632d4e6c
Münir Erkar <munir.erkar@hotmail.com>
2026-01-03 02:10:48 +0300

    Grafik renklendirme ve iş akışı düzeltmeleri tamamlandı.

--- app/controllers/status_tracker_controller.rb
+++ app/controllers/status_tracker_controller.rb
@@ -2,5 +2,8 @@ class StatusTrackerController < ApplicationController
   unloadable
   def index
     @issues = Issue.order('created_on DESC').limit(10)
+    counts = Issue.group(:status).count
+    @chart_labels = counts.map { |status, count| status.name }
+    @chart_data = counts.map { |status, count| count }
   end
 end
